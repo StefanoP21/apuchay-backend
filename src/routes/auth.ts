1d813d04a43f9a1b4ed7a30086e100beb20a6ba9
Stefano <stefanop21@outlook.es>
2024-04-27 16:27:28 -0500

    update: rutas de usuario con validación de campos

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,50 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
 import { createUser, loginUser, renewToken } from '../controllers';
 import { validateJwt, validateFields } from '../middlewares';
 
 export const authRouter = Router();
 
-authRouter.post('/register', validateFields, createUser);
+authRouter.post(
+  '/register',
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('lastname', 'El apellido es obligatorio').not().isEmpty(),
+    check('number', 'El número es obligatorio').not().isEmpty(),
+    check('number', 'El número no es válido').isNumeric(),
+    check('number', 'El número debe tener 9 dígitos').isLength({
+      min: 9,
+      max: 9,
+    }),
+    check('email', 'El correo es obligatorio').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check(
+      'password',
+      'La contraseña debe tener al menos 6 caracteres'
+    ).isLength({
+      min: 6,
+    }),
+    validateFields,
+  ],
+  createUser
+);
 
-authRouter.post('/login', validateFields, loginUser);
+authRouter.post(
+  '/login',
+  [
+    check('email', 'El correo es obligatorio').not().isEmpty(),
+    check('email', 'El correo no es válido').isEmail(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check(
+      'password',
+      'La contraseña debe tener al menos 6 caracteres'
+    ).isLength({
+      min: 6,
+    }),
+    validateFields,
+  ],
+  loginUser
+);
 
 authRouter.get('/renew', validateJwt, renewToken);
