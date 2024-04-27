ec803b6a1c6d41c45b6fbee92fc3d11f5ef1500f
Stefano <stefanop21@outlook.es>
2024-04-27 10:20:20 -0500

    update: token en el controlador

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { User } from '../schema';
 import { IUser } from '../interfaces';
+import { generateJwt } from '../utils';
 
 export const createUser = async (req: Request, res: Response) => {
   const { email, password }: IUser = req.body;
@@ -18,18 +19,20 @@ export const createUser = async (req: Request, res: Response) => {
 
     user = new User(req.body);
 
-    // Encrypt Password
+    //* Encrypt Password
     const salt = bcrypt.genSaltSync();
     user.password = bcrypt.hashSync(password, salt);
 
     await user.save();
 
-    //! Generate JWT
+    //* Generate JWT
+    const token = await generateJwt(user.id, user.name);
 
     res.status(201).json({
       ok: true,
       uid: user.id,
       name: user.name,
+      token,
     });
   } catch (error) {
     console.log(error);
