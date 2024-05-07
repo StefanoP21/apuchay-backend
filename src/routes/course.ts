28390e91a65a665886f5f4010d38948f2d452770
Stefano <stefanop21@outlook.es>
2024-05-06 22:47:21 -0500

    update: validación de token en ruta de cursos por código

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { createCourse, getCourseById, getCourses } from '../controllers';
+import { validateJwt } from '../middlewares';
 
 export const courseRouter = Router();
 
 courseRouter.get('/', getCourses);
 
-courseRouter.get('/:id', getCourseById);
+courseRouter.get('/:id', validateJwt, getCourseById);
 
 courseRouter.post('/new', createCourse);
